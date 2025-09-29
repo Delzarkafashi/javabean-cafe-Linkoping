d1356428bde3f05e45d562b9e57a769751aade80
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-09-29 10:55:36 +0200

    feat: change to close.svg on click

--- olleJS.js
+++ olleJS.js
@@ -1,6 +1,15 @@
 const menu = document.getElementById("menu");
 const hamburgerButton = document.getElementById("hamburger");
+const menuButton = document.getElementById("menu-button")
+let openStatus = false;
 
 hamburgerButton.addEventListener('click', (e) => {
-    menu.classList.add("menu-visible")
+    if (!openStatus) {
+        menuButton.src = "./close.svg";
+        openStatus = true;
+    } else {
+        menuButton.src = "./hamburger.svg";
+        openStatus = false;
+    }
+    menu.classList.toggle("menu-visible");
 });
